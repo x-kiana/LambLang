d01e9f169f9fa7dd35d6f09193cb22b9c84462a8
x-kiana <kiana47@student.ubc.ca>
2024-02-29 12:51:20 -0800

    add comments

--- lib/Ewe.hs
+++ lib/Ewe.hs
@@ -13,6 +13,7 @@ module Ewe(
 import Control.Applicative
 import Data.Char
 
+-- Parser t a is a computation that transforms tokens of type t into the output type a
 data Parser t a where
   Fail :: Parser t a
   Pure :: a -> Parser t a
@@ -28,13 +29,13 @@ instance Functor (Parser t) where
 
 instance Applicative (Parser t) where
   pure = Pure
-
   (<*>) = App
 
 instance Alternative (Parser t) where
   empty = Fail
   (<|>) = Choice
 
+-- run given parser on given list of tokens, return Nothing or Just of pair of the output of type a and remaining tokens
 evalParser :: Parser t a -> [t] -> Maybe (a, [t])
 evalParser Fail _ = Nothing
 evalParser (Pure a) ts = Just (a, ts)
@@ -52,23 +53,31 @@ evalParser (App pf pa) ts = do
   (a, ts) <- evalParser pa ts
   pure (f a, ts)
 
+-- run the given parser on a list of tokens, return Nothing or Just of the output
+-- calls evalParser and ignores the remaining tokens
 runParser :: Parser t a -> [t] -> Maybe a
 runParser p ts = fst <$> evalParser p ts
 
+-- given an input of type a, produce the parser of type Parser a a
 tok :: Eq a => a -> Parser a a
 tok = Tok
 
+-- given an input of type list of a, produce a parser over list of a's
 string :: Eq a => [a] -> Parser a [a]
 string = foldr ((<*>) . ((:) <$>)) (pure []) . map tok
 
+-- given a function from a to boolean as input, produce parser over a
 is :: (a -> Bool) -> Parser a a
 is = Is
 
+-- parser to parse zero or more whitespaces
 whitespace :: Parser Char ()
 whitespace = many (is isSpace) *> pure ()
 
+-- parser to parse one or more whitespaces
 whitespace1 :: Parser Char ()
 whitespace1 = some (is isSpace) *> pure ()
 
+-- parser that always fails to parse
 failP :: Parser t a
 failP = Fail
--- lib/Parse.hs
+++ lib/Parse.hs
@@ -7,21 +7,27 @@ import DataTypes
 import Data.Char
 import Control.Applicative
 
+-- wrapper function over the runParser function imported from Ewe
+-- given a string, calls runParser exprP on the string, returns Either Right Expr if valid or an error
 parse :: String -> Either String Expr
 parse s =
   case runParser exprP s of
     Nothing -> Left "Parse error: invalid LambLang expression"
     Just exp -> Right exp
 
+-- return true if the given char is a valid variable character
 varChar :: Char -> Bool
 varChar = isAlpha
 
+-- parse a string which is a valid variable
 varP :: Parser Char String
 varP = some (is varChar)
 
+-- parse an expression
 exprP :: Parser Char Expr
 exprP = precedenceP [ annP, ioP, appP, ioRet ] argP
 
+-- parse an IOReturn Expr
 ioRet :: Parser Char Expr -> Parser Char Expr
 ioRet p = (do 
   string "return"
@@ -29,24 +35,29 @@ ioRet p = (do
   ret <- p
   pure (IOReturn ret)) <|> p
 
+-- parse an IOBind Expr
 ioP :: Parser Char Expr -> Parser Char Expr
 ioP p = do 
   f <- p
   args <- many (whitespace *> string ">>=" *> whitespace *> p)
   pure (mkRAssocOp IOBind f args)
 
+-- parse an Expr that is either a variable, an expression in parentheses, anonymous function, string, or unit
 argP :: Parser Char Expr
 argP = foldr (<|>) failP [ Var <$> varP, parensP exprP, lamP, stringP, unitP ]
 
+-- given a parser, parse parentheses around it
 parensP :: Parser Char a -> Parser Char a
 parensP p = tok '(' *> whitespace *> p <* whitespace <* tok ')'
 
+-- parse function application
 appP :: Parser Char Expr -> Parser Char Expr
 appP p = do
   f <- p 
   args <- many (whitespace *> p)
   pure (foldl App f args)
 
+-- parse anonymous function
 lamP :: Parser Char Expr
 lamP = do
   tok '\\'
@@ -58,6 +69,7 @@ lamP = do
   body <- exprP
   pure (Lam x body)
 
+-- parse annotation expression
 annP :: Parser Char Expr -> Parser Char Expr
 annP p = do
   exp <- p
@@ -69,9 +81,7 @@ annP p = do
     pure (Just t)) <|> pure Nothing
   pure (maybe exp (Ann exp) maybeT)
 
-typP :: Parser Char Type
-typP = precedenceP [funTP, ioTP] argTP
-
+-- parse a string literal
 stringP :: Parser Char Expr
 stringP = do
   tok '"'
@@ -79,15 +89,21 @@ stringP = do
   tok '"'
   pure (StrLit s)
 
+-- parse a unit literal
 unitP :: Parser Char Expr
 unitP = do
-  tok '('
-  tok ')'
+  string "()"
   pure (UnitLit)
 
+-- parse a type
+typP :: Parser Char Type
+typP = precedenceP [funTP, ioTP] argTP
+
+-- parse a Type that is either a string, unit, or a type in parentheses
 argTP :: Parser Char Type
 argTP = stringTP <|> unitTP <|> parensP typP
 
+-- parse a function type
 funTP :: Parser Char Type -> Parser Char Type
 funTP p = do
   dom <- p
@@ -95,6 +111,7 @@ funTP p = do
   codoms <- many (whitespace *> string "->" *> whitespace *> p)
   pure (mkRAssocOp FunT dom codoms)
 
+-- parse an IO type
 ioTP :: Parser Char Type -> Parser Char Type
 ioTP p = (do
   string "IO"
@@ -102,15 +119,23 @@ ioTP p = (do
   t <- p
   pure (IOT t)) <|> p
 
+-- parse a string type
 stringTP :: Parser Char Type
 stringTP = string "String" *> pure StrT
 
+-- parse a unit type
 unitTP :: Parser Char Type
 unitTP = string "Unit" *> pure UnitT
 
+-- takes a list of precendence parsers and a base parser, and returns
+-- a parser where the first precedence parser in the list has lowest precedence
+-- and the last one has highest.
+-- precedenceP [pp1, ..., ppn] pb = pp1 $ ... $ ppn $ pb
 precedenceP :: [Parser t a -> Parser t a] -> (Parser t a) -> Parser t a
 precedenceP ps p = foldr ($) p ps
 
+-- make a right associative operator
+-- mkRAssocOp op a [b1, ..., bn] = a `op` (b1 `op` ... `op` bn)
 mkRAssocOp :: (a -> a -> a) -> a -> [a] -> a
 mkRAssocOp f a [] = a
 mkRAssocOp f a (hd : tl) = f a (mkRAssocOp f hd tl)
